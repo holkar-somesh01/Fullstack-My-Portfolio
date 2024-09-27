4535f2ddb95f9236305f6e31351565d5880c5fa5
someshwarsholkar <someshwarsholkar22@gmail.com>
2024-09-27 12:35:34 +0530

    FrontEnd Add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
+const path = require("path")
 const { adminProtected } = require("./middleware/Protected")
 require("dotenv").config({ path: "./.env" })   // env File Path 
 
@@ -10,13 +11,15 @@ const app = express()
 app.use(cors({ origin: "http://localhost:5173", credentials: true}))
 app.use(cookieParser())
 app.use(express.json())
+app.use(express.static("dist"))
 
 app.use("/api/auth", require("./routes/auth.routes"))
 app.use("/api/admin",adminProtected, require("./routes/admin.routes"))
 app.use("/api/public",require("./routes/public.routes"))
 
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "Resours Not found" })
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+    // res.status(404).json({ message: "Resours Not found" })
 })
 app.use((err, req, res, next) => {
     console.log(err)
