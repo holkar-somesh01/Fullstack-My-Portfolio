a78fe272e6397587dfcb5c4b1f4471ff76a70900
someshwarsholkar <someshwarsholkar22@gmail.com>
2024-09-27 12:20:26 +0530

    Public Product Fetch Fix

--- routes/public.routes.js
+++ routes/public.routes.js
@@ -4,6 +4,7 @@ const publicController = require("../controller/public.controller")
 router 
     .get("/fetch-project",publicController.fetchProjects)
     .get("/fetch-caro", publicController.getAllCarousel)
+    .get("/project-details/:id", publicController.getProjectDetail)
       
 module.exports = router
      
