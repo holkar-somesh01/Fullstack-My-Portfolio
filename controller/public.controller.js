0656524e2a0b0bb94f85da1f9acd3ccf442fb07d
someshwarsholkar <someshwarsholkar22@gmail.com>
2024-10-01 14:40:02 +0530

    Enqury Message Add

--- controller/public.controller.js
+++ controller/public.controller.js
@@ -1,6 +1,9 @@
 const asyncHandler = require("express-async-handler")
+const Validator = require("validator")
 const Projects = require("../models/Projects")
 const Carousel = require("../models/Carousel")
+const Enquery = require("../models/Enquery")
+const { checkEmpty } = require("../utils/cheackEmpty")
 
 exports.fetchProjects = asyncHandler(async (req, res) => {
     const result = await Projects.find()
@@ -14,4 +17,34 @@ exports.getProjectDetail = asyncHandler(async (req, res) => {
     const {id }=req.params
     const result = await Projects.findById(id)
     res.status(200).json({ message: "Project Details Fetch success", result })
+})
+
+// Enquery
+
+exports.fetchEnqueryMessage = asyncHandler(async (req, res) => {
+    const result = await Enquery.find()
+    res.json({message:"Enquery Message Fetch Success...!", result})
+})
+exports.AddEnqueryMessage = asyncHandler(async (req, res) => {
+    const {name, email, mobile, message, company} = req.body 
+    const { isError, error } = checkEmpty({ name, email, mobile, message, company }) 
+    if (isError) {
+        return res.status(400).json({message:"All Fields Required",error})
+    }
+    if (!Validator.isEmail(email)) {
+        return res.status(400).json({message:"Invalid Email"})
+    }
+    if (!Validator.isMobilePhone(mobile,"en-IN")) {
+        return res.status(400).json({message:"Invalid Mobile"})
+    }
+    await Enquery.create({name, email, mobile, message, company})
+    res.json({message:"Enquery Message Added Success...!",})
+})
+exports.updateEnqueryMessage = asyncHandler(async (req, res) => {
+    await Enquery.findByIdAndUpdate(req.params.id,req.body)
+    res.json({message:"Enquery Message Updated Success...!",})
+})
+exports.deleteEnqueryMessage = asyncHandler(async (req, res) => {
+    await Enquery.findByIdAndDelete(req.params.id,)
+    res.json({message:"Enquery Message Delete Success...!",})
 })
--- routes/public.routes.js
+++ routes/public.routes.js
@@ -5,6 +5,11 @@ router
     .get("/fetch-project",publicController.fetchProjects)
     .get("/fetch-caro", publicController.getAllCarousel)
     .get("/project-details/:id", publicController.getProjectDetail)
-      
+    
+    .get("/fetch-enquery", publicController.fetchEnqueryMessage)
+    .post("/add-enquery", publicController.AddEnqueryMessage)
+    .put("/update-enquery/:id", publicController.updateEnqueryMessage)
+    .delete("/delete-enquery/:id", publicController.deleteEnqueryMessage)
+
 module.exports = router
      
