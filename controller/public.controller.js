13a2b84539f93aaf8c6ef1b3e7599ad448da2c70
someshwarsholkar <someshwarsholkar22@gmail.com>
2024-10-04 12:56:12 +0530

    Enquery Email Send Fix

--- controller/public.controller.js
+++ controller/public.controller.js
@@ -4,17 +4,18 @@ const Projects = require("../models/Projects")
 const Carousel = require("../models/Carousel")
 const Enquery = require("../models/Enquery")
 const { checkEmpty } = require("../utils/cheackEmpty")
+const sendEmail = require("../utils/email")
 
 exports.fetchProjects = asyncHandler(async (req, res) => {
     const result = await Projects.find()
-    res.json({message:"Project Fetch Success...!", result})
+    res.json({ message: "Project Fetch Success...!", result })
 })
 exports.getAllCarousel = asyncHandler(async (req, res) => {
     const result = await Carousel.find()
     res.status(200).json({ message: "blog fetch success", result })
 })
 exports.getProjectDetail = asyncHandler(async (req, res) => {
-    const {id }=req.params
+    const { id } = req.params
     const result = await Projects.findById(id)
     res.status(200).json({ message: "Project Details Fetch success", result })
 })
@@ -23,28 +24,38 @@ exports.getProjectDetail = asyncHandler(async (req, res) => {
 
 exports.fetchEnqueryMessage = asyncHandler(async (req, res) => {
     const result = await Enquery.find()
-    res.json({message:"Enquery Message Fetch Success...!", result})
+    res.json({ message: "Enquery Message Fetch Success...!", result })
 })
 exports.AddEnqueryMessage = asyncHandler(async (req, res) => {
-    const {name, email, mobile, message, company} = req.body 
-    const { isError, error } = checkEmpty({ name, email, mobile, message, company }) 
+    const { name, email, mobile, message, company } = req.body
+    const { isError, error } = checkEmpty({ name, email, mobile, message, company })
     if (isError) {
-        return res.status(400).json({message:"All Fields Required",error})
+        return res.status(400).json({ message: "All Fields Required", error })
     }
     if (!Validator.isEmail(email)) {
-        return res.status(400).json({message:"Invalid Email"})
+        return res.status(400).json({ message: "Invalid Email" })
     }
-    if (!Validator.isMobilePhone(mobile,"en-IN")) {
-        return res.status(400).json({message:"Invalid Mobile"})
+    if (!Validator.isMobilePhone(mobile, "en-IN")) {
+        return res.status(400).json({ message: "Invalid Mobile" })
     }
-    await Enquery.create({name, email, mobile, message, company})
-    res.json({message:"Enquery Message Added Success...!",})
+    sendEmail({
+        to: process.env.MY_EMAIL,
+        message: `Company: ${company}, email: ${email}, mobile: ${mobile}, message: ${message}`,
+        subject: `New Enquery From ${company}`
+    })
+    sendEmail({
+        to: email,
+        message: `Thank You For Enquery. I will get in touch with you Soon `,
+        subject: `Thank You For your intrest`
+    })
+    await Enquery.create({ name, email, mobile, message, company })
+    res.json({ message: "Enquery Message Added Success...!", })
 })
 exports.updateEnqueryMessage = asyncHandler(async (req, res) => {
-    await Enquery.findByIdAndUpdate(req.params.id,req.body)
-    res.json({message:"Enquery Message Updated Success...!",})
+    await Enquery.findByIdAndUpdate(req.params.id, req.body)
+    res.json({ message: "Enquery Message Updated Success...!", })
 })
 exports.deleteEnqueryMessage = asyncHandler(async (req, res) => {
     await Enquery.findByIdAndDelete(req.params.id,)
-    res.json({message:"Enquery Message Delete Success...!",})
+    res.json({ message: "Enquery Message Delete Success...!", })
 })
