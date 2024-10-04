834f36faeb26687c55989bdf8a2ce2ea01b5f6b0
someshwarsholkar <someshwarsholkar22@gmail.com>
2024-10-04 13:19:42 +0530

    Enquery Email Send env variable add Fix

--- controller/public.controller.js
+++ controller/public.controller.js
@@ -39,7 +39,7 @@ exports.AddEnqueryMessage = asyncHandler(async (req, res) => {
         return res.status(400).json({ message: "Invalid Mobile" })
     }
     await sendEmail({
-        to: "someshwarsholkar22@gmail.com",
+        to: process.env.MY_EMAIL,
         message: `Company: ${company}, email: ${email}, mobile: ${mobile}, message: ${message}`,
         subject: `New Enquery From ${company}`
     })
